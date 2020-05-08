07cb41ce35b0675000eb90b1d67cfe5936ea9369
Giovani Henrique Ceron <giovani_ti@hotmail.com>
2020-05-08 13:28:14 -0300

    Ajustado connection para verificar se esta fazendo a conexao corrtamente

--- api/src/config/connection.js
+++ api/src/config/connection.js
@@ -1,7 +1,8 @@
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 let models = {};
+
 const connection = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -12,15 +13,21 @@ const connection = new Sequelize(
   }
 );
 
-const diretorio = path.join(__dirname, '../model');
-fs.readdirSync(diretorio).forEach(file => {
-  const modelDir = path.join(diretorio, file);
-  const model = require(modelDir);
-  model.load(connection);
-  models[model.name] = model;
-});
+connection.authenticate().then(conn => {
+  console.log('Connection Sussessfuly');
 
-console.log(models);
+  const diretorio = path.join(__dirname, '../model');
+  fs.readdirSync(diretorio).forEach(file => {
+    const modelDir = path.join(diretorio, file);
+    const model = require(modelDir);
+    model.load(connection);
+    models[model.name] = model;
+  });
+
+  console.log(models);
+}).catch(error => {
+  console.log('Unable tp connect in database', error);
+});
 
 module.exports = connection;
 
--- api/src/config/server.js
+++ api/src/config/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-require('dotenv');
+require('dotenv').config();
 require('./connection');
 
 app.use(express.json());
